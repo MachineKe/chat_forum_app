3393797951ee34afc0e0f6efce91c99e57a43a55
machineke <markarapsoi72@gmail.com>
2025-05-20 11:54:23 +0300

    add login redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import Forum from "../pages/Forum";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forum" element={<Forum />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
